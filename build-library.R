b246406c202470a799b4cc019ad8f715e75209c8
Carl Boettiger <cboettig@gmail.com>
2016-08-29 21:31:35 +0000

    fixes to build-library.R

--- build-library.R
+++ build-library.R
@@ -2,17 +2,20 @@ library("appl")
 
 ### SETUP ###
 
+log_dir = "."
+
 ## choose the state/action/obs space
-states <- 0:50
+states <- 0:20
 actions <- states
 obs <- states
 
 ## Create a data frame with 1 row for each parameter set for which you want to generate alpha vectors:
-
-models = expand.grid(C = seq(0, 15, 5), sigma_m = c(0.02, 0.05))
+Ks <- data.frame(K = 10:15, r = 1)
+rs <- data.frame(K=15, r = seq(0.6, 1.6, by =0.2))
 
 ## Bind this to a data.frame listing eahc of the fixed parameters across all runs
-models <- data.frame(model = "allen", r = 1, K = 40, sigma_g = 0.02, discount = 0.99, precision = 0.01, models)
+fixed <- data.frame(model = "ricker", sigma_g = 0.1, sigma_m = 0.1, discount = 0.95, precision = 0.1, C = NA)
+models <- data.frame(rbind(Ks, rs), fixed)
 
 ## Preview the model list
 models
@@ -33,17 +36,18 @@ memory <- round(0.95 * as.numeric(gsub(".* (\\d+) .*", "\\1", system("cat /proc/
 for(i in 1:dim(models)[1]) {
   f <- switch(models[i, "model"], 
     allen = appl:::allen(models[i, "r"], models[i, "K"], models[i, "C"]),
-    ricker = appl:::allen(models[i, "r"], models[i, "K"])
+    ricker = appl:::ricker(models[i, "r"], models[i, "K"])
   )
 
   m <- fisheries_matrices(states, actions, obs, reward_fn, f = f, 
                           sigma_g = models[i, "sigma_g"], sigma_m  = models[i, "sigma_m"])
   log_data <- data.frame(model = models[i, "model"], r = models[i, "r"], K  = models[i, "K"], 
-                         C = models[i, "C"], sigma_g = models[i, "sigma_g"], sigma_m = models[i, "sigma_m"])
+                         C = models[i, "C"], sigma_g = models[i, "sigma_g"], sigma_m = models[i, "sigma_m"],
+                         memory = memory)
   
     alpha <- sarsop(m$transition, m$observation, m$reward, 
                     discount = models[i, "discount"], 
                     precision = models[i, "precision"], memory = memory,
-                    log_dir = ".", log_data = log_data)
+                    log_dir = log_dir, log_data = log_data)
 
 }
