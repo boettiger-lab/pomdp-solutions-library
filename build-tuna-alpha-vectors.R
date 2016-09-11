88d775a5cf8581d03b2ef34cade31834951b1f37
Carl Boettiger <cboettig@gmail.com>
2016-09-11 02:11:50 -0400

    tuna alpha vectors

---
+++ build-tuna-alpha-vectors.R
@@ -0,0 +1,52 @@
+library("appl")
+
+### SETUP ###
+
+log_dir = "."
+
+## choose the state/action/obs space
+states <- seq(0,1.2, length=100) # Vector of all possible states
+actions <- seq(0,.8, length=100)   # Vector of actions: harvest
+obs <- states
+
+K = 0.9903371
+r = 0.05699246
+sigma_g = 0.01720091
+
+vars <- expand.grid(r = seq(0.05, 0.3, by =0.05), sigma_m = c(0.1, 0.3, 0.6))
+
+## Bind this to a data.frame listing eahc of the fixed parameters across all runs
+fixed <- data.frame(model = "ricker", sigma_g = sigma_g, discount = 0.99, precision = 0.001, K = K, C = NA)
+models <- data.frame(vars, fixed)
+
+## Preview the model list
+models
+
+## Usual assumption at the moment for reward fn
+reward_fn <- function(x,h) pmin(x,h)
+
+### RUN ### 
+
+## Detect available memory (linux servers only)
+memory <- round(0.95 * as.numeric(gsub(".* (\\d+) .*", "\\1", system("cat /proc/meminfo", intern=TRUE)[1])) / 1000)
+
+
+## Compute alphas for the above examples
+for(i in 1:dim(models)[1]) {
+  f <- switch(models[i, "model"], 
+    allen = appl:::allen(models[i, "r"], models[i, "K"], models[i, "C"]),
+    ricker = appl:::ricker(models[i, "r"], models[i, "K"])
+  )
+
+  m <- fisheries_matrices(states, actions, obs, reward_fn, f = f, 
+                          sigma_g = models[i, "sigma_g"], sigma_m  = models[i, "sigma_m"])
+  log_data <- data.frame(model = models[i, "model"], r = models[i, "r"], K  = models[i, "K"], 
+                         C = models[i, "C"], sigma_g = models[i, "sigma_g"], sigma_m = models[i, "sigma_m"],
+                         memory = memory)
+  
+    alpha <- sarsop(m$transition, m$observation, m$reward, 
+                    discount = models[i, "discount"], 
+                    precision = models[i, "precision"], memory = memory,
+                    log_dir = log_dir, log_data = log_data)
+
+}
